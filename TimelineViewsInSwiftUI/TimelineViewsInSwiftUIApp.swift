d56a1fa6de82c909e7d20541bbff1a5cbd2c8ae2
Ramill <ramill_ibragimov@icloud.com>
2022-05-23 17:19:48 +0300

    Initial Commit

---
+++ TimelineViewsInSwiftUI/TimelineViewsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimelineViewsInSwiftUIApp.swift
+//  TimelineViewsInSwiftUI
+//
+//  Created by Ramill Ibragimov on 23.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TimelineViewsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
