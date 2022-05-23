3704a3b2739d8ee3fc65dc35c584f3d00c0c4221
Ramill <ramill_ibragimov@icloud.com>
2022-05-23 17:34:45 +0300

    init

--- TimelineViewsInSwiftUI/ContentView.swift
+++ TimelineViewsInSwiftUI/ContentView.swift
@@ -9,8 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            TimelineView(.animation) { context in
+                let date = context.date
+                let seconds = Calendar.current.component(.second, from: date)
+                
+                Circle()
+                    .trim(from: 0, to: Double(seconds) / 60.0)
+                    .stroke(Color.red, lineWidth: 16)
+            }
+            .padding(80)
+            
+            TimelineView(.periodic(from: .now, by: 3)) { context in
+                let date = context.date
+                let seconds = Calendar.current.component(.second, from: date)
+                
+                Circle()
+                    .trim(from: 0, to: Double(seconds) / 60.0)
+                    .stroke(Color.green, lineWidth: 12)
+            }
+            .rotationEffect(Angle(degrees: 45.0))
+            .padding(60)
+            
+            TimelineView(.periodic(from: .now, by: 3)) { context in
+                let date = context.date
+                let seconds = Calendar.current.component(.second, from: date)
+                
+                Circle()
+                    .trim(from: 0, to: Double(seconds) / 60.0)
+                    .stroke(Color.orange, lineWidth: 6)
+            }
+            .rotationEffect(Angle(degrees: 90))
+            .padding(30)
+        }.padding()
     }
 }
 
